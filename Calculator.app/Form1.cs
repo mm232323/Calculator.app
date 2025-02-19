4d68817f941b0d9d739a1ccbe477afe396b57388
mm232323 <130875143+mm232323@users.noreply.github.com>
2025-02-19 16:44:34 +0200

    update to arrow functions

--- Calculator.app/Form1.cs
+++ Calculator.app/Form1.cs
@@ -31,64 +31,17 @@ public partial class Form1 : Form
         }
     }
 
-    private void but1_Click(object sender, EventArgs e)
-    {
-
-        handle_But_Click(but1);
-    }
-
-    private void but2_Click(object sender, EventArgs e)
-    {
-        handle_But_Click(but2);
-
-    }
-
-    private void but3_Click(object sender, EventArgs e)
-    {
-        handle_But_Click(but3);
-    }
-
-    private void but4_Click(object sender, EventArgs e)
-    {
-        handle_But_Click(but4);
-        
-
-    }
-
-    private void but7_Click(object sender, EventArgs e)
-    {
-        handle_But_Click(but5);
-    }
-
-    private void but6_Click(object sender, EventArgs e)
-    {
-        handle_But_Click(but6);
-    }
-
-    private void but5_Click(object sender, EventArgs e)
-    {
-        handle_But_Click(but7);
-    }
-
-    private void but10_Click(object sender, EventArgs e)
-    {
-        handle_But_Click(but8);
-    }
-
-    private void but9_Click(object sender, EventArgs e)
-    {
-        handle_But_Click(but9);
-    }
-
-    private void but8_Click(object sender, EventArgs e)
-    {
-        handle_But_Click(but10);
-    }
-
-    private void operator1_Click(object sender, EventArgs e)
-    {
-        handle_Operation_Click(operator1);
-    }
+    private void but1_Click(object sender, EventArgs e) => handle_But_Click(but1);
+    private void but2_Click(object sender, EventArgs e) => handle_But_Click(but2);
+    private void but3_Click(object sender, EventArgs e) => handle_But_Click(but3);
+    private void but4_Click(object sender, EventArgs e) => handle_But_Click(but4);
+    private void but7_Click(object sender, EventArgs e) => handle_But_Click(but5);
+    private void but6_Click(object sender, EventArgs e) => handle_But_Click(but6);
+    private void but5_Click(object sender, EventArgs e) => handle_But_Click(but7);
+    private void but10_Click(object sender, EventArgs e) =>  handle_But_Click(but8);
+    private void but9_Click(object sender, EventArgs e) => handle_But_Click(but9);
+    private void but8_Click(object sender, EventArgs e) => handle_But_Click(but10);
+    private void operator1_Click(object sender, EventArgs e) => handle_Operation_Click(operator1);
 
     private void operator2_Click(object sender, EventArgs e)
     {
@@ -107,15 +60,8 @@ public partial class Form1 : Form
             resultBar.Text = "-";
         }
     }
-    private void operator3_Click(object sender, EventArgs e)
-    {
-        handle_Operation_Click(operator3);
-    }
-    private void operator4_Click(object sender, EventArgs e)
-    {
-        handle_Operation_Click(operator4);
-    }
-
+    private void operator3_Click(object sender, EventArgs e) => handle_Operation_Click(operator3);
+    private void operator4_Click(object sender, EventArgs e) => handle_Operation_Click(operator4);
     private void actionTrigger_Click(object sender, EventArgs e)
     {
         if (Evaluator.operation.Length == 0)
